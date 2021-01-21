ad15e613cf64c1e85008cbc5d70d6a370aa48e25
Stein <will.stein@yahoo.com>
2021-01-20 19:30:51 -0800

    Created project, resume, contact views

--- portfoliowebsite/portfolio/views.py
+++ portfoliowebsite/portfolio/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'portfolio/home.html')
+    context = {
+        'view_name': "home",
+    }
+    return render(request, 'portfolio/home.html', context)
+
+def projects(request):
+    context = {
+        'view_name': "projects",
+    }
+    return render(request, 'portfolio/projects.html', context)
+
+def resume(request):
+    context = {
+        'view_name': "resume",
+    }
+    return render(request, 'portfolio/resume.html', context)
+
+def contact(request):
+    context = {
+        'view_name': "contact",
+    }
+    return render(request, 'portfolio/contact.html', context)
