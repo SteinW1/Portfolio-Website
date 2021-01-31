ce4cc9e1f46c6db10671517365aaf2b4d1ac68a1
Stein <will.stein@yahoo.com>
2021-01-30 19:02:53 -0800

    Added alert messages to contact form page

--- portfoliowebsite/portfolio/views.py
+++ portfoliowebsite/portfolio/views.py
@@ -1,28 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views.generic import DetailView
 from .models import Project
-
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect
 from .forms import ContactForm
 
 def home(request):
     context = {
-        'view_name': "home",
+        'portfolio_view_name': "home",
     }
     return render(request, 'portfolio/home.html', context)
 
 def projects_list(request):
     context = {
-        'view_name': "projects",
+        'portfolio_view_name': "projects",
         'project': Project.objects.all(),
     }
     return render(request, 'portfolio/projects_list.html', context)
 
 def resume(request):
     context = {
-        'view_name': "resume",
+        'portfolio_view_name': "resume",
     }
     return render(request, 'portfolio/resume.html', context)
 
@@ -34,19 +32,21 @@ def contact(request):
             from_name = form.cleaned_data['from_name']
             subject = form.cleaned_data['subject']
             from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
+            contact_message = form.cleaned_data['contact_message']
             try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
+                send_mail(subject, contact_message, from_email, ['admin@example.com'])
             except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('portfolio-contact-success')
+                messages.error(request, f'Invalid header found.')
+                return redirect('portfolio-contact')
+            messages.success(request, f'Thank you, your message has been sent!')
+            return redirect('portfolio-contact')
+        else:
+            for error_message in form.messages:
+                messages.error(request, '{error_message}: {form.error_messages.[error_message]}')
         print(form.errors)
     else:
         form = ContactForm()
-    return render(request, 'portfolio/contact.html', {'form': form})
-
-def contact_success(request):
-    return HttpResponse('Success! Thank you for your message.')
+    return render(request, 'portfolio/contact.html', {'form': form, 'portfolio_view_name': "contact"})
 
 class ProjectDetailView(DetailView):
     model = Project
