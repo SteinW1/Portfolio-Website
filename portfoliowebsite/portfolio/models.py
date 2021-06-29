b57e42adf2f37455b1923106f5aed06fb9c6ea26
SteinW1 <will.stein@yahoo.com>
2021-06-29 10:29:19 -0700

    updated project model

--- portfoliowebsite/portfolio/models.py
+++ portfoliowebsite/portfolio/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import random
 
 class Project(models.Model):
-    project_name = models.CharField(max_length=25, primary_key=True, null=False)
+    id = models.AutoField(primary_key=True)
+    project_name = models.CharField(max_length=25, null=False)
     short_description = models.CharField(max_length=200, null=False)
     long_description = models.TextField(blank=True)
     github_link = models.URLField(blank=True)
