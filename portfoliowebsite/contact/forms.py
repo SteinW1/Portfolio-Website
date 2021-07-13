28305a9a6f89f182780de81c95079c667aed84d8
SteinW1 <will.stein@yahoo.com>
2021-07-12 19:26:22 -0700

    improved responsive css and added favicon

--- portfoliowebsite/contact/forms.py
+++ portfoliowebsite/contact/forms.py
@@ -5,26 +5,26 @@ class ContactForm(forms.Form):
         required=True,
         label='Name:',
         widget=forms.TextInput(
-            attrs={'class':'form-textfield'}
+            attrs={'class':'contact-form-textfield'}
         ))
         
     from_email = forms.EmailField(
         required=True,
         label='Email:', 
         widget=forms.TextInput(
-            attrs={'class':'form-textfield'})
+            attrs={'class':'contact-form-textfield'})
         )
         
     subject = forms.CharField(
         required=True,
         label='Subject:',
         widget=forms.TextInput(
-            attrs={'class':'form-textfield'})
+            attrs={'class':'contact-form-textfield'})
         )
         
     contact_message = forms.CharField(
         required=True,
         label='Message:',
         widget=forms.Textarea(
-            attrs={'class':'form-textfield'})
+            attrs={'class':'contact-form-textfield'})
         )
