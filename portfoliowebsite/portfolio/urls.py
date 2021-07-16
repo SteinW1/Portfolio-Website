0605cd2a421cf508f21c9eb277ec8b78fa2da33f
SteinW1 <will.stein@yahoo.com>
2021-07-15 18:10:55 -0700

    minor changes

--- portfoliowebsite/portfolio/urls.py
+++ portfoliowebsite/portfolio/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from .views import ProjectDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home , name='portfolio-home'),
+    path('', views.home, name='portfolio-home'),
     path('projects/', views.projects_list, name='portfolio-projects'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
     path('resume/', views.resume, name='portfolio-resume'),
